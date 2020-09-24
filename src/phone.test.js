ac0da1e1e29f64cff971e314abaa2c82da148fc3
lindsayRae <lbarnett712@gmail.com>
2020-09-24 06:53:53 -0400

    fix - new RegEx for isPhoneNumber, added tests

--- src/phone.test.js
+++ src/phone.test.js
@@ -1,7 +1,56 @@
 import {isPhoneNumber} from './phone'
-// HW - rewrite this
 
-test('Match string to regex', () => {
-    expect(functions.isPhoneNumber('555-5555', /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/gm))
-    .toBe(true)
+describe('isPhoneNumber basic functionality', () => {
+    
+    test('isPhoneNumber is true when I pass 1234567890', ()=> {
+        expect(isPhoneNumber('1234567890')).toBe(true)
+    }) 
+
+    test('isPhoneNumber is true when I pass 123-456-7890', ()=> {
+        expect(isPhoneNumber('123-456-7890')).toBe(true)
+    })
+
+    test('isPhoneNumber is true when I pass 123.456.7890', ()=> {
+        expect(isPhoneNumber('123.456.7890')).toBe(true)
+    })  
+    
+    test('isPhoneNumber is true when I pass (123) 456-7890', ()=> {
+        expect(isPhoneNumber('(123) 456-7890')).toBe(true)
+    }) 
+
+    test('isPhoneNumber is true when I pass 123 456 7890', ()=> {
+        expect(isPhoneNumber('123 456 7890')).toBe(true)
+    }) 
+
+    test('isPhoneNumber is true when I pass (123)456-7890', ()=> {
+        expect(isPhoneNumber('(123)456-7890')).toBe(true)
+    })          
+    
+    test('isPhoneNumber is false when I pass 123-4567', ()=> {
+        expect(isPhoneNumber('123-4567')).toBe(false)
+    })
+
+    test('isPhoneNumber is false when I pass null', () => {
+        expect(isPhoneNumber(null)).toBe(false)
+    })
+
+    test('isPhoneNumber is false when I pass an object', () => {
+        expect(isPhoneNumber({})).toBe(false)
+    })
+
+    test('isPhoneNumber is false when I pass an array', () => {
+        expect(isPhoneNumber([])).toBe(false)
+    })
+
+    test('isPhoneNumber is false when I pass an empty string', () => {
+        expect(isPhoneNumber('')).toBe(false)
+    })
+
 })
+
+describe('isPhoneNumber with country code "1"', () => {
+    test('isPhoneNumber is false when I pass 1-555-555-5555', ()=> {
+        expect(isPhoneNumber('1-555-555-5555')).toBe(false)
+    }) 
+})
+
